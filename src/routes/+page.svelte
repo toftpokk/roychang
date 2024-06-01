4d896a53199808aeebea8b6e3434da4c2dd7131f
toftpokk <pakkapol.20100@gmail.com>
2024-06-01 22:03:37 +0700

    Front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1 class="bg-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col h-full">
+    <div class="min-h-[92px] max-h-[20em] flex flex-col h-full">
+        <h1 class="text-[4em] text-center mt-auto max-h-[92px] font-serif leading-[0.7em]">Roychang</h1>
+    </div>
+    <p class="text-center text-xl text-gray-500">Thai Recipes, Simply</p>
+    <div class="my-4">
+        <form action="/search">
+            <div class="flex max-w-[584px] border border-black px-2 py-1.5 rounded-lg mx-auto">
+                <div class="me-2">
+                    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">
+                        <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"/>
+                    </svg>
+                </div>
+                <div class="flex-grow">
+                    <input class="w-full text-[16px] outline-none"/>
+                </div>
+            </div>
+            <p class="mx-auto text-center text-gray-600 leading-10">Search for ingredients, menus, styles</p>
+        </form>
+    </div>
+    <div class="grow"></div>
+    <div class="mx-auto">This software is licensed under the MIT License. Creator: toftpokk</div>
+</div>
